f3035abd661a15605854c63306798ee35c2e3d76
Nicoly Malescza <Nicolysilvamalescza@gmail.com>
2023-09-25 17:37:11 -0300

    0.2

--- Prova3B/Administrativo.cs
+++ Prova3B/Administrativo.cs
@@ -10,18 +10,20 @@ namespace Prova3B
     {
         public double Transporte { get; set; }
         public double Alimentacao { get; set; }
+        public string Funcao { get; set; }
 
         public Administrativo() { }
-        public Administrativo(string nome, string cpf, string matricula, DateTime dataNasc, string sexo, double salario, double transporte, double alimentacao) : base(nome, cpf, matricula, dataNasc, sexo, salario)
+        public Administrativo(string nome, string cpf, string matricula, DateTime dataNasc, string sexo, double salario, double transporte, double alimentacao, string funcao) : base(nome, cpf, matricula, dataNasc, sexo, salario)
         {
             Transporte = transporte;
             Alimentacao = alimentacao;
+            Funcao = funcao;
         }
 
         public override double Beneficio(double salario)
         {
-            //double valeTransporte = 150;
-            return salario + 0.15;
+            double cont = salario + 0.15;
+            return cont + 150;
         }
     }
 }
